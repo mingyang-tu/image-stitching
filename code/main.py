2b27a9befee250f316d3c92440300e8b09462235
MY Tu <tu890626@gmail.com>
2023-04-28 13:50:20 +0800

    add images

--- code/main.py
+++ code/main.py
@@ -3,18 +3,18 @@ from lib import image_stitching
 
 
 if __name__ == "__main__":
-    root = "../dataset/parrington/"
+    root = "../image/"
     names = [
-        f"prtn{i:02d}.jpg" for i in range(18)
+        f"img{i}.JPG" for i in range(9)
     ]
 
     images = []
     for i in names:
-        images.append(cv2.imread(root + i))
+        images.append(cv2.resize(cv2.imread(root + i), (0, 0), fx=1/4, fy=1/4))
 
-    result = image_stitching(images, 704.916)
+    result = image_stitching(images, 800)
 
-    # cv2.imwrite("../result.jpg", result)
+    cv2.imwrite("../result.png", result)
 
     cv2.imshow("Result", result)
     cv2.waitKey()
